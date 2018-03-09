10ace2a88c255cf0b80c0a97ee9c1c7f44d0f7f5
Marissa Baden <badenm42@gmail.com>
2018-03-09 05:03:57 -0700

    Fix unecessary div around input.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,9 +4,7 @@ import './Form.css';
 class Form extends Component {
     render () {
         return (
-            <div className='firm'>
-                <input type="text"  name='message' value={this.props.value} placeholder={this.props.placeholder} onChange={(event) => this.props.changed(event)} onKeyPress={this.props.onKeyPress}></input>
-            </div>
+            <input type="text"  name='message' value={this.props.value} placeholder={this.props.placeholder} onChange={(event) => this.props.changed(event)} onKeyPress={this.props.onKeyPress}></input>
         ); 
     }
 }
